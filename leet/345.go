693cd60985b7c7cc20ad778e608bca1a218f1f6d
tolluset <dlqud19@gmail.com>
2023-10-30 23:48:03 +0900

    wip: leet 345.go

--- leet/345.go
+++ leet/345.go
@@ -3,22 +3,27 @@ package main
 import (
   "fmt"
   // "strings"
+"slices"
 )
 
 func main() {
-  fmt.Println(reverseVowels("hello"))
+  fmt.Println("res", reverseVowels("hello"))
 }
 
 func reverseVowels(s string) string {
-  sv := []rune("aeiouAEIOU")
-  runes := []rune(s)
+  vowels := []rune("aeiouAEIOU")
+  s_runes := []rune(s)
+  s_vowels := []rune{}
   
 
   for i := 0; i < len(s); i++ {
-
       fmt.Println(len(s), len(runes))
-    fmt.Println(s[i])
-    fmt.Println(sv)
+    // fmt.Println(s[i])
+    fmt.Println(vowels)
+    fmt.Println(runes)
+    if (slices.Contains(vowels, s[i])) {}
+      s_vowels = append(s_vowels, s[i])
+    }
   }
   return s
 }
